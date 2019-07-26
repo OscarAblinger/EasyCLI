2972c556c13fefb53af28306ebdb72e3a6955d7a
SourceOverflow <oscarablinger@gmail.com>
2019-07-27 00:39:30 +0200

    feat(api): Add DuplicateCommandException

---
+++ EasyCLI/DuplicateCommandException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EasyCli
+{
+    public class DuplicateCommandException : Exception
+    {
+        public DuplicateCommandException()
+        {
+        }
+
+        public DuplicateCommandException(string message) : base(message)
+        {
+        }
+
+        public DuplicateCommandException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
