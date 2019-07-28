159ce4ba655d0be5fa4c94b012a6407381cb1600
SourceOverflow <oscarablinger@gmail.com>
2019-07-28 19:22:53 +0200

    fix(api): Fix formatting of CliFactory

--- EasyCLI/CliFactory.cs
+++ EasyCLI/CliFactory.cs
@@ -1,11 +1,19 @@
 ﻿using EasyCli.Impl;
+using System;
+using System.Collections.Generic;
 
 namespace EasyCli
 {
     public static class CliFactory
     {
-        public static ICli Create() => new Cli(CreateDefaultConfig());
+        public static ICli Create()
+        {
+            return new Cli(CreateDefaultConfig());
+        }
 
-        public static IConfiguration CreateDefaultConfig() => new Configuration();
+        public static IConfiguration CreateDefaultConfig()
+        {
+            return new Configuration();
+        }
     }
 }
