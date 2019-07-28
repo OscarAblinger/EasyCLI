3f495b7d261e4d08627cb42469510dca15dd42ab
SourceOverflow <oscarablinger@gmail.com>
2019-07-28 14:15:36 +0200

    feat(api): Add basic configuration interface

--- EasyCLI/IConfiguration.cs
+++ EasyCLI/IConfiguration.cs
@@ -1,6 +1,19 @@
-﻿namespace EasyCli
+﻿using EasyCli.ConsoleInterface;
+using System;
+using System.Collections.Generic;
+
+namespace EasyCli
 {
+    public delegate void ExceptionCatchHandler(Exception ex);
+
     public interface IConfiguration
     {
+        IConsoleIO IOImplementation { get; set; }
+
+        Either<string, Action<ICli>> Greeting { get; set; }
+        Either<string, Action<ICli>> Prompt { get; set; }
+
+        ExceptionCatchHandler ExceptionCatchHandler { get; set; }
+        IList<ICommand> Commands { get; set; }
     }
 }
