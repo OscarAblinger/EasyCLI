0e1b71e66ede285ce202a984389c9e61aa16f14f
SourceOverflow <oscarablinger@gmail.com>
2019-07-28 13:36:38 +0200

    feat(api): Add Cursor interface

---
+++ EasyCLI/ConsoleInterface/ICursor.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyCli.ConsoleInterface
+{
+    public interface ICursor
+    {
+        int CursorLeft { get; set; }
+        int CursorTop { get; set; }
+        int CursorSize { get; set; }
+        int CursorVisible { get; set; }
+    }
+}
