8e7a7ee27000ccfb20c335b0abef13242e279436
SourceOverflow <oscarablinger@gmail.com>
2019-07-28 19:26:00 +0200

    feat(cli-base): Implement Configuration struct

--- EasyCLI/impl/Configuration.cs
+++ EasyCLI/impl/Configuration.cs
@@ -1,6 +1,15 @@
-﻿namespace EasyCli.Impl
+﻿using System;
+using System.Collections.Generic;
+using EasyCli.ConsoleInterface;
+
+namespace EasyCli.Impl
 {
     internal class Configuration : IConfiguration
     {
+        public IConsoleIO IOImplementation { get; set; }
+        public OneOf<string, string[], Action<ICli>> Greeting { get; set; }
+        public Either<string, Action<ICli>> Prompt { get; set; }
+        public ExceptionCatchHandler ExceptionCatchHandler { get; set; }
+        public IList<ICommand> Commands { get; set; }
     }
 }
