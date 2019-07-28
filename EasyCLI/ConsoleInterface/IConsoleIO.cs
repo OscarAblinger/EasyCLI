c029e532dd6136b63416d37993bce057fbf9b794
SourceOverflow <oscarablinger@gmail.com>
2019-07-28 13:36:59 +0200

    feat(api): Add IO combined interface

---
+++ EasyCLI/ConsoleInterface/IConsoleIO.cs
@@ -0,0 +1,28 @@
+﻿namespace EasyCli.ConsoleInterface
+{
+    public interface IConsoleIO
+    {
+        IInput In { get; }
+        IOutput Out { get; }
+        ICursor Cursor { get; }
+
+        /// <summary>
+        /// Remove a number of already printed characters.
+        /// 
+        /// This method may do nothing
+        /// </summary>
+        /// <param name="number"></param>
+        void DeleteCharacters(int number);
+
+        /// <summary>
+        /// Gets the width of the buffer area.
+        /// Setter not available as some OS don't offer support for that
+        /// </summary>
+        int BufferHeight { get; }
+        /// <summary>
+        /// Gets the height of the buffer area.
+        /// Setter not available as some OS don't offer support for that
+        /// </summary>
+        int BufferWidth { get; }
+    }
+}
